7e378132caad061c97055ec85a964f7b776738c1
Jack Mu <ok@JACKMU-TECH.JM>
2025-06-09 02:01:52 +0300

    Updated the Urls for tasks

--- smart_study_planner/urls.py
+++ smart_study_planner/urls.py
@@ -2,8 +2,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('tasks/', include('tasks.urls')),
+    path('', include('tasks.urls')),
+    
 ]
